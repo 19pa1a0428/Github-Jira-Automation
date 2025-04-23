c4aaa0ce05cd50af337e20870370cdcaa961bcfe
Venkata Rahul Chinta <60500456+19pa1a0428@users.noreply.github.com>
2025-04-22 21:28:38 -0500

    Update create_jira.py

--- create_jira.py
+++ create_jira.py
@@ -1,1 +1,54 @@
-#Python script for creating jira
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "#link for your attlassian jira portal"
+
+API_TOKEN = "Your api token"
+
+auth = HTTPBasicAuth("", API_TOKEN)
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "My first jira ticket",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "project": {
+      "key": "AB"
+    },
+    "issuetype": {
+      "id": "10006"
+    },
+    "summary": "First JIRA Ticket",
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
